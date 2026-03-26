89ca1e7ac3b841769cacc8f5c9a134be31a1d5e6
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-03-25 21:53:58 -0300

    feat: create `Product` entity

---
+++ src/main/java/com/adriel/product_api/model/Product.java
@@ -0,0 +1,31 @@
+package com.adriel.product_api.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private BigDecimal price;
+    private String description;
+    private String productIdentifier;
+}
